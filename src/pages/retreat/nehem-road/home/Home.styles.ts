985996a89f6c2c6352497bd3980d574503571d1e
HyeongjaeLee <tlwprosper@gmail.com>
2025-07-10 03:39:14 +0900

    feat : 홈 화면 추가

---
+++ src/pages/retreat/nehem-road/home/Home.styles.ts
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+import { Image } from 'antd';
+import PosterMain from 'assets/images/nehem-road/poster_main.png';
+import PosterMainPc from 'assets/images/nehem-road/poster_main_pc.png';
+
+export const Wrapper = styled.div<{ ismobile: string }>`
+  display: flex;
+  flex-direction: column;
+  width: ${({ ismobile }) => (ismobile === 'true' ? '100%' : '60%')};
+  margin: 0 auto;
+`;
+
+export const PosterWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
+
+export const Poster = styled(Image).attrs((props) => ({
+  width: '100%',
+  src: PosterMain,
+  preview: false,
+}))``;
+
+export const PosterPc = styled(Image).attrs((props) => ({
+  width: '100%',
+  src: PosterMainPc,
+  preview: false,
+}))``;
+
+export const DescriptionWrapper = styled.div<{ ismobile: string }>`
+  width: 100%;
+  padding-left: ${({ ismobile }) => (ismobile === 'true' ? '20px' : '')};
+  padding-right: ${({ ismobile }) => (ismobile === 'true' ? '20px' : '')};
+  padding-bottom: 50px;
+  color: #ccc;
+`;
+
+export const DescriptionTitle = styled.p<{ ismobile: string }>`
+  width: 100%;
+  margin-top: 30px;
+  margin-bottom: 4px;
+  font-size: ${({ ismobile }) => (ismobile === 'true' ? '18px' : '24px')};
+`;
+
+export const Description = styled.p<{ ismobile: string }>`
+  width: 100%;
+  margin: 0;
+  font-size: ${({ ismobile }) => (ismobile === 'true' ? '14px' : '18px')};
+`;
+
+export const DescriptionTitleLarge = styled.p<{ ismobile: string }>`
+  width: 100%;
+  margin-top: 30px;
+  margin-bottom: 4px;
+  font-size: ${({ ismobile }) => (ismobile === 'true' ? '20px' : '30px')};
+  color: #fff;
+`;
+
+export const DescriptionLarge = styled.p<{ ismobile: string }>`
+  width: 100%;
+  margin-bottom: 4px;
+  font-size: ${({ ismobile }) => (ismobile === 'true' ? '20px' : '30px')};
+  color: #fff;
+`;
