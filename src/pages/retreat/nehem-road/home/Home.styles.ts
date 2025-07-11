213e2bd85f39c51f7b2e3e81d6ded88dc9a36fcc
HyeongjaeLee <tlwprosper@gmail.com>
2025-07-11 11:24:51 +0900

    fix : transient props 적용해서 console warning 제거

--- src/pages/retreat/nehem-road/home/Home.styles.ts
+++ src/pages/retreat/nehem-road/home/Home.styles.ts
@@ -3,10 +3,10 @@ import { Image } from 'antd';
 import PosterMain from 'assets/images/nehem-road/poster_main.png';
 import PosterMainPc from 'assets/images/nehem-road/poster_main_pc.png';
 
-export const Wrapper = styled.div<{ ismobile: string }>`
+export const Wrapper = styled.div<{ $ismobile: string }>`
   display: flex;
   flex-direction: column;
-  width: ${({ ismobile }) => (ismobile === 'true' ? '100%' : '60%')};
+  width: ${({ $ismobile }) => ($ismobile === 'true' ? '100%' : '60%')};
   margin: 0 auto;
 `;
 
@@ -28,50 +28,50 @@ export const PosterPc = styled(Image).attrs((props) => ({
   preview: false,
 }))``;
 
-export const DescriptionWrapper = styled.div<{ ismobile: string }>`
+export const DescriptionWrapper = styled.div<{ $ismobile: string }>`
   width: 100%;
-  padding-left: ${({ ismobile }) => (ismobile === 'true' ? '20px' : '')};
-  padding-right: ${({ ismobile }) => (ismobile === 'true' ? '20px' : '')};
+  padding-left: ${({ $ismobile }) => ($ismobile === 'true' ? '20px' : '')};
+  padding-right: ${({ $ismobile }) => ($ismobile === 'true' ? '20px' : '')};
   padding-bottom: 50px;
   color: #ccc;
 `;
 
-export const DescriptionCategory = styled.p<{ ismobile: string; mt?: number }>`
+export const DescriptionCategory = styled.p<{ $ismobile: string; $mt?: number }>`
   width: 100%;
-  margin-top: ${({ mt }) => (mt ? mt : 30)}px;
+  margin-top: ${({ $mt }) => ($mt ? $mt : 30)}px;
   margin-bottom: 4px;
-  font-size: ${({ ismobile }) => (ismobile === 'true' ? '20px' : '26px')};
+  font-size: ${({ $ismobile }) => ($ismobile === 'true' ? '20px' : '26px')};
   font-weight: bold;
   color: #f0a721;
 `;
 
-export const DescriptionTitle = styled.p<{ ismobile: string; mt?: number }>`
+export const DescriptionTitle = styled.p<{ $ismobile: string; $mt?: number }>`
   width: 100%;
-  margin-top: ${({ mt }) => (mt ? mt : 30)}px;
+  margin-top: ${({ $mt }) => ($mt ? $mt : 30)}px;
   margin-bottom: 4px;
-  font-size: ${({ ismobile }) => (ismobile === 'true' ? '18px' : '24px')};
+  font-size: ${({ $ismobile }) => ($ismobile === 'true' ? '18px' : '24px')};
   font-weight: bold;
 `;
 
-export const Description = styled.p<{ ismobile: string }>`
+export const Description = styled.p<{ $ismobile: string }>`
   width: 100%;
   margin: 0;
-  font-size: ${({ ismobile }) => (ismobile === 'true' ? '14px' : '18px')};
+  font-size: ${({ $ismobile }) => ($ismobile === 'true' ? '14px' : '18px')};
 `;
 
-export const DescriptionTitleLarge = styled.p<{ ismobile: string }>`
+export const DescriptionTitleLarge = styled.p<{ $ismobile: string }>`
   width: 100%;
   margin-top: 30px;
   margin-bottom: 4px;
-  font-size: ${({ ismobile }) => (ismobile === 'true' ? '20px' : '30px')};
+  font-size: ${({ $ismobile }) => ($ismobile === 'true' ? '20px' : '30px')};
   font-weight: bold;
   color: #fff;
 `;
 
-export const DescriptionLarge = styled.p<{ ismobile: string }>`
+export const DescriptionLarge = styled.p<{ $ismobile: string }>`
   width: 100%;
   margin-bottom: 4px;
-  font-size: ${({ ismobile }) => (ismobile === 'true' ? '20px' : '30px')};
+  font-size: ${({ $ismobile }) => ($ismobile === 'true' ? '20px' : '30px')};
   font-weight: bold;
   color: #fff;
 `;
