fd9c7c351e724b00b4cf80033edc21872084c1c6
HyeongjaeLee <tlwprosper@gmail.com>
2026-02-19 02:10:56 +0900

    feat : 채팅 목록 작업중

--- src/pages/prayer-altar/PrayerAltar.tsx
+++ src/pages/prayer-altar/PrayerAltar.tsx
@@ -1,9 +1,77 @@
 import { useQuery } from '@tanstack/react-query';
-import { useEffect, useState, useMemo } from 'react';
+import { useState, useMemo, useRef } from 'react';
 import { queries } from 'api/queries';
 import PrayerAltarImage from './PrayerAltarImage';
+import { Avatar, Comment, List, Skeleton } from 'antd';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { CommentData, CommentOptions } from 'types';
+import { UserOutlined } from '@ant-design/icons';
+import moment from 'moment';
 
 const PrayerAltar = () => {
+  /** Hook */
+  const commentListRef = useRef<HTMLDivElement>(null);
+
+  /** State */
+  const [commentData, setCommentData] = useState<CommentData>({
+    totalCount: 0,
+    comments: [],
+  });
+  const [commentOptions, setCommentOptions] = useState<CommentOptions>({
+    offset: 0,
+    limit: 10,
+  });
+
+  // 댓글 목록 조회 API
+  const {
+    data: commentListQueryData = { items: [], total: 0 },
+    refetch: refetchCommentList,
+    isSuccess: commentListQuerSuccess,
+    isFetching: commentListFetching,
+  } = useQuery({
+    ...queries.comment.list({
+      offset: commentOptions.offset,
+      limit: commentOptions.limit,
+    }),
+    staleTime: 500,
+    cacheTime: 1000,
+  });
+
+  // 댓글 목록 데이터 세팅
+  useMemo(() => {
+    if (commentListQuerSuccess) {
+      const queryData = [...commentListQueryData.items];
+      const newData = queryData?.map((item) => {
+        return {
+          author: item.user_name,
+          avatar: (
+            <Avatar
+              icon={<UserOutlined />}
+              // style={{
+              //   backgroundColor: colorCodeList[random(0, colorCodeList.length)],
+              // }}
+            />
+          ),
+          content: <p>{item.content}</p>,
+          datetime: moment(item.created_at).format('YYYY-MM-DD HH:mm:ss'),
+        };
+      });
+      setCommentData({
+        ...commentData,
+        comments: [...((commentOptions?.offset && commentData?.comments) || []), ...newData],
+        totalCount: commentListQueryData?.total,
+      });
+    }
+  }, [commentListQueryData]);
+
+  // 댓글 스크롤
+  const handleScroll = async () => {
+    setCommentOptions((prev) => ({
+      ...prev,
+      offset: commentOptions.offset + commentOptions.limit,
+    }));
+  };
+
   return (
     <>
       <div className='prayer-altar-wrap'>
@@ -23,7 +91,23 @@ const PrayerAltar = () => {
         <div className='chat-title-wrap'>
           <div className='chat-title' />
         </div>
-        <div className='chat-content'></div>
+        <div className='chat-content'>
+          <div id='infinite-scroll-comment' ref={commentListRef}>
+            <InfiniteScroll
+              dataLength={commentData?.comments?.length}
+              next={handleScroll}
+              hasMore={commentData?.comments?.length < commentData?.totalCount}
+              loader={<Skeleton avatar paragraph={{ rows: 1 }} active />}
+              scrollableTarget='infinite-scroll-comment'
+            >
+              <List
+                dataSource={commentData?.comments}
+                itemLayout='horizontal'
+                renderItem={(props) => <Comment {...props} />}
+              />
+            </InfiniteScroll>
+          </div>
+        </div>
       </div>
     </>
   );
